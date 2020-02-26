f285d2197b4d05bed5d73dfcdc627af127babf86
NuarkNoir <mrcluster@ya.ru>
2020-02-26 16:18:02 +0700

    Add text keeping functionality

--- nottee_bot.php
+++ nottee_bot.php
@@ -17,7 +17,82 @@ $bot->cmd("/start", "Hello, I am note-taking bot! You can get help with `/help` 
 
 $bot->cmd("/help",  "Use `{id} <linebreak> {your text}` to save something (to use in chats - ".
                     "prepend {id} with `/note`).\n".
+                    "Note: {id} wil be of spaces/tabs, inner spaces will become underscores.\n".
                     "Then reference it with `/ref {id}`.\n".
                     "Also you can list them all with `/list`");
+
+$bot->on("text", function() use ($db) {
+    $message = Bot::message();
+    if (substr($message["text"], 0, 6) === "/note ") {
+        $message["text"] = substr($message["text"], 6);
+    }
+    else if ($message["chat"]["type"] != "private") {
+        return "ok";
+    }
+
+    $result = "";
+    $pieces = explode("\n", $message["text"], 2);
+    if (count($pieces) !== 2) {
+        $result = "Cannot parse your note. Right format is `{id} <linebreak> {text}`.";
+    }
+    else {
+        $chatid = $message["chat"]["id"];
+        $ownerid = $message["from"]["id"];
+        $type = "text";
+        list($refid, $data) = $pieces;
+        $refid = str_ireplace(" ", "_", trim($refid));
+        $noteid = "{$chatid}_{$ownerid}_{$refid}";
+        $note = $db->note->getOrCreate([
+            "noteid" => $noteid, 
+            "chatid" => $chatid, 
+            "ownerid" => $ownerid
+        ]);
+        if (isset($note->ref_id)) {
+            $note->ref->data = $data;
+            $note->ref->save();
+            $result = "Note with `$refid` id updated";
+        }
+        else {
+            $new_ref = $db->ref->create(["type" => $type, "data" => $data])->save();
+            $note->ref_id = $new_ref->id;
+            $result = "Note saved with `$refid` id";
+        }
+        $note->save();
+    }
+
+    return Bot::sendMessage($result);
+});
+
+$bot->cmd("/ref",  function($refid) use ($db) {
+    $message = Bot::message();
+    $result = "";
+
+    $chatid = $message["chat"]["id"];
+    $ownerid = $message["from"]["id"];
+    $refid = str_ireplace(" ", "_", trim($refid));
+    $noteid = "{$chatid}_{$ownerid}_{$refid}";
+    print_r($noteid);
+
+    $note = $db->note->get([
+        "noteid" => $noteid, 
+        "chatid" => $chatid, 
+        "ownerid" => $ownerid
+    ]);
+
+    if (isset($note)) {
+        if (isset($note->ref_id)) {
+            $ref = $note->ref;
+            $result = $ref->data;
+        }
+        else {
+            $result = "Somehow, you got empty ref. Please, contact @nuark about this incident.";
+        }
+    }
+    else {
+        $result = "Note with id `$refid` not exists";
+    }
+
+    return Bot::sendMessage($result);
+});
 
 $bot->run();
